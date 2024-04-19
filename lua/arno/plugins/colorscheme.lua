8243b7e53215958c2712263371a9721e9f351e4a
Arno Vanneste <arno.vanneste@marelec.com>
2024-04-19 09:30:03 +0200

    Removed unused var

--- lua/arno/plugins/colorscheme.lua
+++ lua/arno/plugins/colorscheme.lua
@@ -5,7 +5,6 @@ return {
 	lazy = false,
 	priority = 1000,	-- make sure to load this before all the other start plugins
 	init = function()
-	  colorscheme = catppuccin
 	  vim.cmd.colorscheme("catppuccin")
 	end,
 }
